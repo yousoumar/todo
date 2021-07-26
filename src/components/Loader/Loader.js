f8d1f2a45329df7389ca0747e9121f8588d085ab
yousoumar <oumaiyr@gmail.com>
2021-07-26 17:20:09 +0200

    adds a loader animation

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,12 @@
-import './Loader.css'
+import './Loader.css';
+import circle from './circle.svg';
 
 export default function Loader() {
     return (
         <div className = "loader">
             <h1>#todo</h1>
-            <div>pour ne plus jamais oublier</div>
+            <div>Pour jamais oublieR</div>
+            <div className="circle"><img src={circle} alt="" /></div>
         </div>
     )
 }
